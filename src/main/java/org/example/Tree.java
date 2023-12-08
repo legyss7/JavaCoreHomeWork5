745f676aaffe876c669e9c275db8e49c6fc2e488
legyss7 <legyss@mail.ru>
2023-12-08 13:37:49 +0600

    added Tree

---
+++ src/main/java/org/example/Tree.java
@@ -0,0 +1,57 @@
+package org.example;
+
+import java.io.File;
+
+public class Tree {
+
+    public static void main(String[] args) {
+        print(new File("/home/ol/projects/JavaProjects/AndroidGB/JavaCore/homeWork5/src"), "", "|", true);
+    }
+
+    /**
+     * Метод печатает полноценное дерево директории
+     * @param file путь к директории
+     * @param indent отступы
+     * @param indicator индикатор (файл или папка)
+     * @param isLast флаг следующего элемента дерева
+     */
+    static void print(File file, String indent, String indicator, boolean isLast){
+        System.out.print(indent);
+        if (isLast){
+            System.out.print("└─");
+            indent += "  ";
+        }
+        else {
+            System.out.print("├─");
+            indent += "│ ";
+        }
+        System.out.println(indicator + file.getName() + indicator);
+
+        File[] files = file.listFiles();
+        if (files == null)
+            return;
+
+        int subDirTotal = 0;
+        for (File element : files) {
+            if (element.isDirectory())
+                subDirTotal++;
+        }
+
+        int subFileTotal = 0;
+        for (File item : files) {
+            if (item.isFile())
+                subFileTotal++;
+        }
+
+        int subDirCounter = 0;
+        int subFileCounter = 0;
+        for (File value : files) {
+            if (value.isDirectory()) {
+                print(value, indent, indicator,subDirTotal == ++subDirCounter);
+            } else {
+                print(value, indent, "", subFileTotal == ++subFileCounter);
+            }
+        }
+    }
+
+}
